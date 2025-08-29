95daf48d60048afbd020a7a529c7f611db28a149
pazgonzalezherra <p.gonzalezherra@gmail.com>
2025-08-29 15:23:10 -0300

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,9 @@
+//declaracion de variables
+let boton = document.getElementById("btntexto");
+let parrafo = document.getElementById("texto");
+boton.addEventListener("click", function() {
+    parrafo.innerHTML = "Texto modificado";
+});
+//Funcion para modificar el parrafo
+GamepadButton.addEventListener("click",function)parrafo
+            
